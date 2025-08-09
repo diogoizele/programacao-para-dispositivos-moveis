7a3ee2b7792acb3d4bab878a016abf0ecddfd5ad
diogoizele <diogogabrielizele@gmail.com>
2025-08-09 15:57:58 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'programacao-para-dispositivos-moveis',
+  webDir: 'www'
+};
+
+export default config;
